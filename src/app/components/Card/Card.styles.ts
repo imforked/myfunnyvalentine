7ecc623a698c0e8438091dcc7523e065184709b4
imforked <10011101.dev@gmail.com>
2026-01-15 17:03:57 -0800

    wip: flip card

--- src/app/components/Card/Card.styles.ts
+++ src/app/components/Card/Card.styles.ts
@@ -1,4 +1,7 @@
-import { styled } from "styled-components";
+import { styled, css } from "styled-components";
+
+const CARD_BORDER_RADIUS = 12;
+export const FLIP_TIME_IN_MS = 800;
 
 export const Wrapper = styled.div`
   display: flex;
@@ -13,5 +16,32 @@ export const Container = styled.div`
   position: relative;
   width: 400px;
   height: 560px;
-  background: red;
+  border-radius: ${CARD_BORDER_RADIUS}px;
+  perspective: 800px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const Front = styled.div<{
+  $showFront: boolean;
+  $showFrontShadow: boolean;
+}>`
+  background: linear-gradient(145deg, #ff4d4d, #cc0000);
+  border: 2px solid #900;
+  width: 100%;
+  height: 100%;
+  transition: transform ${FLIP_TIME_IN_MS}ms, box-shadow ${FLIP_TIME_IN_MS}ms;
+  transform-style: preserve-3d;
+  z-index: 2;
+  border-radius: ${CARD_BORDER_RADIUS}px;
+  box-shadow: ${({ $showFront }) => ($showFront ? "10px" : "-10px")} 5px 20px
+    rgba(0, 0, 0, 0.5);
+
+  ${({ $showFront }) =>
+    !$showFront &&
+    css`
+      transform: rotateY(180deg);
+    `}
 `;
--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,10 +1,26 @@
+"use client";
+
+import { useState } from "react";
 import * as S from "./Card.styles";
 
 export const Card = () => {
+  const [showFront, setShowFront] = useState(true);
+  const [showFrontShadow, setShowFrontShadow] = useState(true);
+
+  const handleFlip = () => {
+    setShowFront(!showFront);
+    // setTimeout(() => {
+    //   setShowFrontShadow(!showFront);
+    // }, S.FLIP_TIME_IN_MS / 1.75);
+  };
+
   return (
     <S.Wrapper>
-      <S.Container>
-        <h1>lmao</h1>
+      <S.Container onClick={handleFlip}>
+        <S.Front
+          $showFront={showFront}
+          $showFrontShadow={showFrontShadow}
+        ></S.Front>
       </S.Container>
     </S.Wrapper>
   );
