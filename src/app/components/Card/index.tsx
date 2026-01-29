456bc700cbc668d2f01476043ea03c2e4dc9b8ad
imforked <10011101.dev@gmail.com>
2026-01-28 18:19:22 -0800

    Cancel unlocking features if Kari doesn't hold down long enough.

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -2,8 +2,6 @@
 
 import { useState, useRef } from "react";
 import * as S from "./styles/Card.styles";
-// import { Sticker } from "../Sticker";
-// import { STICKER_CONTEXT } from "../Sticker/Sticker.context";
 import { CardProps } from "./Card.types";
 import { Tabs } from "../Tabs";
 
@@ -17,8 +15,21 @@ export const Card = ({ onStickerClick }: CardProps) => {
   const [featuresUnlocked, setFeaturesUnlocked] = useState(false);
   const [shakeCard, setShakeCard] = useState(false);
 
-  const timeoutRef = useRef<number | null>(null);
+  const pressTimeoutRef = useRef<number | null>(null);
+  const shakeTimeoutRef = useRef<number | null>(null);
+  const unlockTimeoutRef = useRef<number | null>(null);
+
   const ignoreNextClickRef = useRef(false);
+
+  const clearAllTimeouts = () => {
+    if (pressTimeoutRef.current) clearTimeout(pressTimeoutRef.current);
+    if (shakeTimeoutRef.current) clearTimeout(shakeTimeoutRef.current);
+    if (unlockTimeoutRef.current) clearTimeout(unlockTimeoutRef.current);
+
+    pressTimeoutRef.current = null;
+    shakeTimeoutRef.current = null;
+    unlockTimeoutRef.current = null;
+  };
 
   const clickHandler = () => {
     if (ignoreNextClickRef.current) {
@@ -29,67 +40,45 @@ export const Card = ({ onStickerClick }: CardProps) => {
   };
 
   const mouseDownHandler = () => {
-    timeoutRef.current = window.setTimeout(() => {
+    pressTimeoutRef.current = window.setTimeout(() => {
       setIsBeingTouched(true);
 
-      setTimeout(() => {
-        setTimeout(
+      shakeTimeoutRef.current = window.setTimeout(() => {
+        setShakeCard(true);
+        ignoreNextClickRef.current = true;
+
+        unlockTimeoutRef.current = window.setTimeout(
           () => {
             setFeaturesUnlocked(true);
           },
           S.PRESS_ANIMATION_IN_MS + SHAKE_DURATION + TAB_REVEAL_DELAY,
         );
-
-        setShakeCard(true);
-        // Ignore the next clicks that follow a long press
-        ignoreNextClickRef.current = true;
       }, S.PRESS_ANIMATION_IN_MS + SHAKE_DURATION);
     }, PRESS_ANIMATION_DELAY);
   };
 
   const mouseUpHandler = () => {
     setIsBeingTouched(false);
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-      timeoutRef.current = null;
-    }
+    setShakeCard(false);
+    clearAllTimeouts();
   };
-
-  // const Stickers = ({
-  //   isInteractiveStickers,
-  // }: {
-  //   isInteractiveStickers: boolean;
-  // }) =>
-  //   STICKER_CONTEXT.map((sticker, index) => (
-  //     <Sticker
-  //       key={index}
-  //       {...sticker}
-  //       index={index}
-  //       onClick={onStickerClick}
-  //       isInteractiveSticker={isInteractiveStickers}
-  //     />
-  //   ));
 
   return (
     <S.Wrapper>
       <S.Container
         onMouseDown={mouseDownHandler}
         onMouseUp={mouseUpHandler}
+        onMouseLeave={mouseUpHandler}
+        onTouchStart={mouseDownHandler}
+        onTouchEnd={mouseUpHandler}
         onClick={clickHandler}
         $isBeingTouched={isBeingTouched}
       >
-        {/* {showFront && (
-          <S.InteractiveStickersContainer>
-            <Stickers isInteractiveStickers />
-          </S.InteractiveStickersContainer>
-        )} */}
         <S.ShakeLayer $shake={shakeCard}>
           <S.DriftLayer>
             <S.Flipper $showFront={showFront}>
               <S.Front $showFront={showFront} $isBeingTouched={isBeingTouched}>
-                <S.FrontContent>
-                  {/* <Stickers isInteractiveStickers={false} /> */}
-                </S.FrontContent>
+                <S.FrontContent />
               </S.Front>
 
               <Tabs reveal={featuresUnlocked} />
